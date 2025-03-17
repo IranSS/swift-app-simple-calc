e686cee0e96768af960993efe7e5fe07c97046f0
Iranildo Sousa <iranildosousa1234@gmail.com>
2025-03-17 15:32:35 -0300

    Initial Commit

---
+++ swift-app/swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_appApp.swift
+//  swift-app
+//
+//  Created by user on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
